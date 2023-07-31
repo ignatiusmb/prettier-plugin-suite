7d1c2037fd51dc4a7891d07c1a4cf5bd8213210c
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-07-31 17:04:50 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# prettier-plugin-sort-package-json
+
+A [Prettier](https://prettier.io/) plugin to sort the `package.json` file in a consistent and opinionated way, this ensures that your `package.json` file is always well-organized and easy to read.
+
+- 🌟 Set and forget
+- 🔌 Zero-dependency
+- 🔧 Zero-configuration
+- 🛠️ Auto-fix with Prettier
+- 🚀 No extra commands needed
+- 🧹 Lint together with Prettier
+
+## Usage
+
+To install `prettier-plugin-sort-package-json`, run the following command:
+
+```bash
+pnpm add -D prettier-plugin-sort-package-json
+```
+
+To use `prettier-plugin-sort-package-json`, add it to your Prettier configuration file ([automatic plugin discovery has been removed](https://prettier.io/blog/2023/07/05/3.0.0.html#plugin-search-feature-has-been-removed-14759httpsgithubcomprettierprettierpull14759-by-fiskerhttpsgithubcomfisker)):
+
+```json
+{
+    "plugins": ["prettier-plugin-sort-package-json"]
+}
+```
+
+You can then run Prettier as usual, and your `package.json` files will be sorted automatically.
---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import babel from 'prettier/parser-babel';
+
+export const parsers: Record<string, import('prettier').Parser> = {
+	'json-stringify': {
+		...babel.parsers['json-stringify'],
+		preprocess(text, options) {
+			if (!/[\\/]package\.json$/.test(options.filepath)) {
+				if (!babel.parsers['json-stringify'].preprocess) return text;
+				return babel.parsers['json-stringify'].preprocess(text, options);
+			}
+
+			const original = JSON.parse(text);
+			const sorted = order.reduce(
+				(pkg, k) => {
+					if (k in original) pkg[k] = original[k];
+					return delete original[k], pkg;
+				},
+				{} as Record<string, unknown>,
+			);
+
+			const space = options.useTabs ? '\t' : options.tabWidth;
+			return JSON.stringify({ ...sorted, ...original }, null, space);
+		},
+	},
+};
+
+const order = [
+	'$schema',
+	'private',
+	'name',
+	'version',
+	'description',
+	'repository',
+	'bugs',
+	'homepage',
+	'license',
+	'funding',
+
+	'author',
+	'maintainers',
+	'contributors',
+	'type',
+
+	'scripts',
+	'config',
+	'bin',
+	'main',
+	'module',
+	'browser',
+	'types',
+	'typings',
+
+	'exports',
+	'files',
+	'packageManager',
+	'engineStrict',
+	'engines',
+	'os',
+	'cpu',
+	'prettier',
+
+	'keywords',
+	'dependencies',
+	'peerDependencies',
+	'peerDependenciesMeta',
+	'bundleDependencies',
+	'bundledDependencies',
+	'optionalDependencies',
+	'devDependencies',
+	'overrides',
+
+	'publishConfig',
+	'workspaces',
+	'pnpm',
+];
