f8214195de933203fefbe9a2269c5ed62ac82421
Ignatius Bagus <ignatius.mbs@gmail.com>
2025-06-06 13:56:42 +0700

    add comment

--- prettier.config.js
+++ prettier.config.js
@@ -3,7 +3,10 @@ import mauss from 'mauss/prettier.config.js';
 export default {
 	...mauss,
 	plugins: ['./workspace/prettier-plugin-sort-package-json/src/index.js'],
-	// overrides: mauss.overrides.filter((o) => {
-	// 	return !o.options.plugins?.includes('prettier-plugin-sort-package-json');
-	// }),
+	// the packages in the workspace directory have the exact name of the package
+	// and the detection of the plugin gets confused because of the monorepo structure
+	// so we need to clear the packages from the config, only for this monorepo
+	overrides: mauss.overrides.filter((o) => {
+		return !o.options.plugins?.includes('prettier-plugin-sort-package-json');
+	}),
 };
