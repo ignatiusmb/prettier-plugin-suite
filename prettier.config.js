4402d50024b67f99845f6f66167f92446dd05794
Ignatius Bagus <ignatius.mbs@gmail.com>
2025-06-06 13:42:42 +0700

    ok fine

--- prettier.config.js
+++ prettier.config.js
@@ -3,4 +3,7 @@ import mauss from 'mauss/prettier.config.js';
 export default {
 	...mauss,
 	plugins: ['./workspace/prettier-plugin-sort-package-json/src/index.js'],
+	overrides: mauss.overrides.filter((o) => {
+		return !o.options.plugins?.includes('prettier-plugin-sort-package-json');
+	}),
 };
